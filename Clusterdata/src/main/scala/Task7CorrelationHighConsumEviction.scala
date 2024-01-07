057b78edcd36ee3faf0a9809b46d4e169f3b5ea3
wuzho <zhongyuanwu72@gmail.com>
2024-01-07 02:46:34 +0100

    Q7 modified

--- Clusterdata/src/main/scala/Task7CorrelationHighConsumEviction.scala
+++ Clusterdata/src/main/scala/Task7CorrelationHighConsumEviction.scala
@@ -11,6 +11,7 @@ object Task7CorrelationHighConsumEviction {
         schema_task_events.printTreeString()
         val schema_task_usage = ReadSchema.read("task_usage")
         schema_task_usage.printTreeString()
+
         // Initialize SparkSession
         val sk = SparkSession.builder()
                               .appName("CHCE")
@@ -20,61 +21,45 @@ object Task7CorrelationHighConsumEviction {
         // Set level of log to ERROR
         sk.sparkContext.setLogLevel("ERROR")
 
-        var files_task_events = Files.walk(Paths.get("./data/task_events"), FileVisitOption.FOLLOW_LINKS).toArray
-        var files_task_usage = Files.walk(Paths.get("./data/task_usage"), FileVisitOption.FOLLOW_LINKS).toArray
+        // Read the data files (*.csv)
+        val taskEventsDF = sk.read
+                              .format("csv")
+                              .option("header", "false")
+                              .option("delimiter", ",")
+                              .schema(schema_task_events)
+                              .load("./data/task_events/*.csv")
+        val taskUsageDF = sk.read
+                            .format("csv")
+                            .option("header", "false")
+                            .option("delimiter", ",")
+                            .schema(schema_task_usage)
+                            .load("./data/task_usage/*.csv")
 
-        var df_task_events_var : DataFrame = null
-        var df_task_usage_var : DataFrame = null
+        // Extract the dataframe of the evicted tasks
+        val evictedTasksDF = taskEventsDF.filter(col("Event type") === 2).select("job ID", "task index", "machine ID")
+        // Extract the dataframe of the machines by the descending order of CPU consumption
+        val taskUsageOrderedDF = taskUsageDF.select("machine ID", "CPU rate").orderBy(col("CPU rate").desc).distinct()
+        
+        // Define the threshold of "High consumption"
+        val highConsumRate = 0.0001
+        // Number of machines
+        val numMachines = taskUsageOrderedDF.count()
 
-        for (file <- files_task_events) {
-          val filePath = file.toString()
-          val df : DataFrame = sk.read
-                                  .format("csv")
-                                  .option("header", "false")
-                                  .option("delimiter", ",")
-                                  .schema(schema_task_events)
-                                  .load(filePath)
-          df.show()
-          if (df_task_events_var == null) 
-            df_task_events_var = df
-          else 
-            df_task_events_var = df_task_events_var.union(df)  
-        }
+        // Number of high-consuming machines
+        val top : Int = (numMachines * highConsumRate).toInt
+        // Extract the dataframe of high-consuming machines
+        val topConsumingMachines = taskUsageOrderedDF.limit(top)
 
-        for (file <- files_task_usage) {
-          val filePath = file.toString()
-          val df : DataFrame = sk.read
-                                  .format("csv")
-                                  .option("header", "false")
-                                  .option("delimiter", ",")
-                                  .schema(schema_task_usage)
-                                  .load(filePath)
-          if (df_task_usage_var == null) 
-            df_task_usage_var = df
-          else 
-            df_task_usage_var = df_task_usage_var.union(df)  
-        }
+        // Combine the dataframes, extract the dataframe of high-consuming and evicted tasks
+        val combinedEvictHighComsumDF = topConsumingMachines.join(evictedTasksDF, "machine ID")
+                                                          .select("job ID", "task index", "machine ID")
+                                                          .distinct()
 
-        val taskEventsDF = df_task_events_var
-        val taskUsageDF = df_task_usage_var
-
-        // Task events: Col 6 -> Event type
-        val renamedTaskEventsDF = taskEventsDF.withColumnRenamed("_c5", "Event type")
-
-        // Task usage: Col 6 -> CPU rate (usage)
-        val renamedTaskUsageDF = taskUsageDF.withColumnRenamed("_c5", "CPU rate")
-
-        // Task events: Col 3 -> job ID, Col 4 -> task index, Col 5 -> machine ID
-        // Task usage: Col 3 -> job ID, Col 4 -> task index, Col 5 -> machine ID
-        val combinedDF : DataFrame = renamedTaskEventsDF.join(renamedTaskUsageDF, Seq("_c2", "_c3", "_c4"))
-
-        val avgCPURate : Double = combinedDF.select("CPU rate").agg(avg("CPU rate")).collect()(0)(0).asInstanceOf[Double]
-
-        val evictTask : DataFrame = combinedDF.select("CPU rate").filter(col("Event type") === 2)
-
-        val evictTaskHighConsum : DataFrame = evictTask.filter(col("CPU rate") > avgCPURate)
-
-        println("Total: " + combinedDF.count() + " Evict:" + evictTask.count() + " High Consum Evict: " + evictTaskHighConsum.count())
+        // Print logs
+        println("Total High comusing machines: " + top + ", ")
+        println("Total machines: " + numMachines + ", ")
+        println("Total Evicted Tasks: " + evictedTasksDF.count() + ", ")
+        println("Evicted Task on High Consuming machines: " + combinedEvictHighComsumDF.count())
 
         // Shut down SparkSession
         sk.stop()
