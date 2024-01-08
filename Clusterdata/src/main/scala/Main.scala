11c8ec585ec549c0b8eeb3b89aa38a0f1ec9cfb4
MA Yuqiang <ylasce@gmail.com>
2024-01-08 12:31:18 +0100

    config cloud

--- Clusterdata/src/main/scala/Main.scala
+++ Clusterdata/src/main/scala/Main.scala
@@ -2,22 +2,24 @@ package clusterdata
 
 object Main {
   def main(args: Array[String]): Unit = {
-    if (args.length < 1) {
+    if (args.length < 2) {
       println("[ERROR] Usage: spark-submit target/scala-2.12/clusterdata_2.12-1.0.jar [taskID]")
       System.exit(1)
     }
 
+    val onCloud = if (args(1) == "cloud") true else false
+    
     val taskName = args(0)
     // 根据 taskName 执行相应的任务逻辑
     taskName match {
-      case "task1" => Task1DistributionOfCPU.execute()
-      case "task2" => Task2PercentageOfCompPowerLost.execute()
-      case "task3" => Task3DistributionOfSchedulingClass.execute()
-      case "task4" => Task4SchedulingClassEviction.execute()
-      case "task5" => Task5SameJobMachine.execute()
-      case "task6" => Task6ResourceRequestConsume.execute()
-      case "task7" => Task7CorrelationHighConsumEviction.execute()
-      case "task8" => Task8TaskConsumCPUAndMem.execute()
+      case "task1" => Task1DistributionOfCPU.execute(onCloud)
+      case "task2" => Task2PercentageOfCompPowerLost.execute(onCloud)
+      case "task3" => Task3DistributionOfSchedulingClass.execute(onCloud)
+      case "task4" => Task4SchedulingClassEviction.execute(onCloud)
+      case "task5" => Task5SameJobMachine.execute(onCloud)
+      case "task6" => Task6ResourceRequestConsume.execute(onCloud)
+      case "task7" => Task7CorrelationHighConsumEviction.execute(onCloud)
+      case "task8" => Task8TaskConsumCPUAndMem.execute(onCloud)
       // 添加更多任务...
       case _ =>
         println(s"[ERROR] Unknown task ID: $taskName")
